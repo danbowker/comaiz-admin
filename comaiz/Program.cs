7fb940e9a2a87cbab699ce041e916962b0c414d3
danbowker <danbowker@googlemail.com>
2024-02-17 17:23:23 +0000

    Topic/db/move to cockroachdb (#14)
    * Move to cockroachdb
    
    * Use user secret for connection string
    
    ---------
    
    Co-authored-by: Dan Bowker <dan.bowker@horiba.com>

--- comaiz/Program.cs
+++ comaiz/Program.cs
@@ -1,6 +1,7 @@
 ﻿using comaiz.data;
 using comaiz.data.Services;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 // Required for ExcelDataReader
 System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
@@ -8,9 +9,28 @@ System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Inst
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connStringBuilder = new NpgsqlConnectionStringBuilder();
+connStringBuilder.SslMode = SslMode.VerifyFull;
+
+// TODO: Use appsettings for connection string except for username and password
+// TODO: Use local database for development
+// TODO: Use environment variable for production
+
+// To use, setup an app secret with dotnet user-secrets
+var databaseUrlEnv = builder.Configuration["CockroachDB"];
+
+var databaseUrl = new Uri(databaseUrlEnv);
+connStringBuilder.Host = databaseUrl.Host;
+connStringBuilder.Port = databaseUrl.Port;
+var items = databaseUrl.UserInfo.Split(new[] { ':' });
+if (items.Length > 0) connStringBuilder.Username = items[0];
+if (items.Length > 1) connStringBuilder.Password = items[1];
+
+connStringBuilder.Database = "comaiz";
+
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ComaizContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("comaizContext") ?? throw new InvalidOperationException("Connection string 'comaizContext' not found."), x => x.UseDateOnlyTimeOnly()));
+    options.UseNpgsql(connStringBuilder.ConnectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddSingleton<ExcelAccountsReader>();
