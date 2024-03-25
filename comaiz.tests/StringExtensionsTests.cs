362112b44739299aa37c18352ceb46f003b61c1d
Max Bowker <41957373+maxmax3721@users.noreply.github.com>
2024-03-25 12:33:05 +0000

    String Extensions Class moved to comaiz.data project and unit tests added (#16)

---
+++ comaiz.tests/StringExtensionsTests.cs
@@ -0,0 +1,34 @@
+using Xunit;
+using comaiz.Extensions;
+
+namespace comaiz.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void GetNpgsqlConnectionString_UriFormat_ReturnsExpectedConnectionString()
+        {
+            // Arrange
+            string configuredConnectionString = "postgres://username:password@localhost:5432/comaiz";
+
+            // Act
+            string result = configuredConnectionString.GetNpgsqlConnectionString();
+
+            // Assert
+            Assert.Equal("SSL Mode=VerifyFull;Host=localhost;Port=5432;Username=username;Password=password;Database=comaiz", result);
+        }
+
+        [Fact]
+        public void GetNpgsqlConnectionString_ConnectionStringFormat_ReturnsExpectedConnectionString()
+        {
+            // Arrange
+            string configuredConnectionString = "Host=localhost;Port=5432;Username=username;Password=password;Database=comaiz";
+
+            // Act
+            string result = configuredConnectionString.GetNpgsqlConnectionString();
+
+            // Assert
+            Assert.Equal("Host=localhost;Port=5432;Username=username;Password=password;Database=comaiz", result);
+        }
+    }
+}
