9b764e0b9900d589c8acbc047439e447216c83a3
danbowker <danbowker@googlemail.com>
2025-10-07 18:15:57 +0000

    Pending changes exported from your codespace

--- comaiz.data/Migrations/20250929120102_EFCore9Upgrade.Designer.cs
+++ comaiz.data/Migrations/20250929120102_EFCore9Upgrade.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using comaiz.data;
@@ -11,9 +12,11 @@ using comaiz.data;
 namespace comaiz.data.Migrations
 {
     [DbContext(typeof(ComaizContext))]
-    partial class ComaizContextModelSnapshot : ModelSnapshot
+    [Migration("20250929120102_EFCore9Upgrade")]
+    partial class EFCore9Upgrade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ comaiz.data/Migrations/20250929120102_EFCore9Upgrade.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace comaiz.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EFCore9Upgrade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            // migrationBuilder.DropColumn(
+            //     name: "Discriminator",
+            //     table: "WorkRecord"); // Commented out: column may not exist
+
+
+            // migrationBuilder.DropColumn(
+            //     name: "Discriminator",
+            //     table: "FixedCost"); // Commented out: column may not exist
+
+            // migrationBuilder.DropColumn(
+            //     name: "Discriminator",
+            //     table: "CarJourney");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "WorkRecord",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "FixedCost",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "CarJourney",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
