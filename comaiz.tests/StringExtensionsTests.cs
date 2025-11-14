86ac6fa80f02f90d0216a5ea0c07a66258171f27
danbowker <danbowker@googlemail.com>
2025-11-14 17:39:27 +0000

    Handle non-standard db name (#67)
    Co-authored-by: Dan Bowker <dan.bowker@horiba.com>

--- comaiz.tests/StringExtensionsTests.cs
+++ comaiz.tests/StringExtensionsTests.cs
@@ -9,26 +9,26 @@ namespace comaiz.Tests
         public void GetNpgsqlConnectionString_UriFormat_ReturnsExpectedConnectionString()
         {
             // Arrange
-            string configuredConnectionString = "postgres://username:password@localhost:5432/comaiz";
+            string configuredConnectionString = "postgres://username:password@localhost:5432/my_db";
 
             // Act
             string result = configuredConnectionString.GetNpgsqlConnectionString();
 
             // Assert
-            Assert.Equal("SSL Mode=VerifyFull;Host=localhost;Port=5432;Username=username;Password=password;Database=comaiz", result);
+            Assert.Equal("SSL Mode=VerifyFull;Host=localhost;Port=5432;Database=my_db;Username=username;Password=password", result);
         }
 
         [Fact]
         public void GetNpgsqlConnectionString_ConnectionStringFormat_ReturnsExpectedConnectionString()
         {
             // Arrange
-            string configuredConnectionString = "Host=localhost;Port=5432;Username=username;Password=password;Database=comaiz";
+            string configuredConnectionString = "Host=localhost;Port=5432;Username=username;Password=password;Database=my_db";
 
             // Act
             string result = configuredConnectionString.GetNpgsqlConnectionString();
 
             // Assert
-            Assert.Equal("Host=localhost;Port=5432;Username=username;Password=password;Database=comaiz", result);
+            Assert.Equal("Host=localhost;Port=5432;Username=username;Password=password;Database=my_db", result);
         }
     }
 }
